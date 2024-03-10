e2e0f2ea223c35a8db2fd741815df30aa592147f
Hasanli Munavvar <hesenlimine96@gmail.com>
2024-03-10 18:22:17 +0400

    jjjjj

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world Second Repo!");
+        System.err.println("salam1");
+        System.err.println("salam2");
     }
+
 }
